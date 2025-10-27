d79b250ec5e0543efc02632c8208f0c6c45b3c22
Ojcitrius32 <732537@student.dusd.net>
2025-10-27 13:58:48 -0700

    Added another color
    blue works now

--- P6HosangScriptingProgect/Assets/Scripts/BehaviourScript.cs
+++ P6HosangScriptingProgect/Assets/Scripts/BehaviourScript.cs
@@ -20,12 +20,12 @@ public class BehaviourScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.G))
         {
             GetComponent<Renderer>().material.color = Color.green;
-            {
-                if (Input.GetKeyDown(KeyCode.B))
+        }
+               if (Input.GetKeyDown(KeyCode.B))
                 {
                     GetComponent<Renderer>().material.color = Color.blue;
                 }
             }
         }
-    }
-}
+    
+
